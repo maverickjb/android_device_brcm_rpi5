a74dc205010ecc31ecabb94e419c736dbb2895f2
maverick <maverick_jia@sina.com>
2025-05-13 17:31:56 +0800

    debug graphics

--- device.mk
+++ device.mk
@@ -29,6 +29,22 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
+	android.hardware.composer.hwc3-service.drm \
+	android.hardware.graphics.allocator-service.minigbm_gbm_mesa \
+	mapper.minigbm_gbm_mesa \
+	libgbm_mesa_wrapper
+
+PRODUCT_PACKAGES += \
+	libEGL_mesa \
+	libGLESv1_CM_mesa \
+	libGLESv2_mesa \
+	libgallium_dri
+
+PRODUCT_PACKAGES += \
+	dri_gbm \
+	libgbm_mesa
+
+PRODUCT_PACKAGES += \
 	android.hardware.graphics.composer3-V3-ndk \
 	android.hardware.graphics.allocator@2.0 \
 	android.hardware.graphics.allocator@3.0 \
@@ -41,10 +57,22 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.common@1.0 \
 	android.hardware.graphics.common@1.1 \
 	android.hardware.graphics.common@1.2 \
-	android.hardware.graphics.common-V5-ndk \
-	libgralloctypes \
-	libdmabufheap \
-	libdrm
+	android.hardware.graphics.common-V5-ndk
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
+
+PRODUCT_PACKAGES += \
+	vulkan.broadcom
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
+## Health
+PRODUCT_PACKAGES += \
+	android.hardware.health-service.batteryless
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -62,4 +90,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/ramdisk/fstab.rpi5:$(TARGET_COPY_OUT_RAMDISK)/fstab.rpi5 \
     $(DEVICE_PATH)/ramdisk/fstab.rpi5:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.rpi5 \
-    $(DEVICE_PATH)/ramdisk/init.rpi5.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rpi5.rc \
+    $(DEVICE_PATH)/ramdisk/init.rpi5.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rpi5.rc
+
